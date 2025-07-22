68ff95d7fe7a64bfd08cde32edcb584c08b5f664
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-07-21 22:24:16 -0700

    adding curl reference

--- frontend/src/composables/useWeatherSocket.js
+++ frontend/src/composables/useWeatherSocket.js
@@ -1,3 +1,28 @@
+// const curel = {
+//   coord: { lon: -117.3677, lat: 47.6166 },
+//   weather: [{ id: 804, main: 'Clouds', description: 'overcast clouds', icon: '04n' }],
+//   base: 'stations',
+//   main: {
+//     temp: 61.36,
+//     feels_like: 60.71,
+//     temp_min: 58.32,
+//     temp_max: 63.34,
+//     pressure: 1011,
+//     humidity: 75,
+//     sea_level: 1011,
+//     grnd_level: 933,
+//   },
+//   visibility: 10000,
+//   wind: { speed: 3.44, deg: 80 },
+//   clouds: { all: 100 },
+//   dt: 1753161159,
+//   sys: { type: 1, id: 5812, country: 'US', sunrise: 1753100033, sunset: 1753155453 },
+//   timezone: -25200,
+//   id: 5811704,
+//   name: 'Spokane',
+//   cod: 200,
+// }
+
 import { ref, onBeforeUnmount } from 'vue'
 
 const weatherDic = {
